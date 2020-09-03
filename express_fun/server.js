33c3ed26bea25cc7affd76bdc8f4d1478781dabb
Josh <jemidlo17@gmail.com>
2020-09-03 18:44:26 -0500

    added pictures to express_fun

--- express_fun/server.js
+++ express_fun/server.js
@@ -1,19 +1,9 @@
 const express = require('express')
-const ejs = require('ejs')
 const app = express()
 
     app.use(express.static(__dirname+'/static'))
-    app.set('view engine', 'ejs')
-    app.set('views', __dirname + '/views')
+    app.set('static', __dirname + '/static')
 
-    app.get('/', (req,res)=>{
-        res.send('Hello Class')
-    })
-    app.get('/home', (req,rep)=>{
 
-        
-
-        res.render('index')
-    })
 
 app.listen(8080, ()=>{console.log('listening on port 8080')})
