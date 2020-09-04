ad8bec9cffbee089655bf3fe55594e0232680b2e
Josh <jemidlo17@gmail.com>
2020-09-03 19:54:32 -0500

    first commit

---
+++ express/ejs_cars_cats/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const ejs = require('ejs')
+const app = express();
+
+app.use(express.static(__dirname + "/static"))
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+app.get('/cars', (req, res) => {
+    res.render('cars')
+})
+
+app.get('/cats', (req, res) => {
+    res.render('cats')
+})
+
+app.get('/form', (req,res)=>{
+    res.render('form')
+}) 
+
+
+
+app.listen(8000, () => { console.log("listening on port 8000") })
